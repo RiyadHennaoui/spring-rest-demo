70c635acb130cf74ee6b4ae119d93c58a7e96499
Riyad Hennaoui <riyad.hennaoui@gmail.com>
2022-04-21 10:50:33 +0200

    Initial commit

---
+++ spring-rest-demo/src/main/java/com/luv2code/json/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.luv2code.json.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.luv2code.json")
+public class DemoAppConfig {
+
+
+}
